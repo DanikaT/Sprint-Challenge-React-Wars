3c79e7ff09968855bacce0f6c1d09c6ef8bd287b
Danika Thomson <danikathomson@Danikas-mac.local>
2020-01-26 03:06:36 +0100

    added state, fetched data, added Characterlist compnent and added it to App.js.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios"
 import './App.css';
+import CharacterList from "./components/CharacterList";
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  const [characters, setCharacters] = useState([]);
+// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+    .then(response => {
+       setCharacters(response.data.results);
+      console.log(response.data);
+    })
+
+  .catch(error => {
+    console.log("Uh ohhh! Where's my data??!!", error);
+});
+
+}, []);
+
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {   
+        
+        (characters.map((person, index) => (
+          <CharacterList 
+          key={index}
+          name={person.name}
+          />
+        )
+  ))
+        }
+      
     </div>
   );
 }
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CharacterList = () => {
+    // Try to think through what state you'll need for this app before starting. Then build out
+    // the state properties here.
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+    // side effect in a component, you want to think about which state and/or props it should
+    // sync up with, if any.
+   return (
+      <div className="App">
+        <h1 className="character-list">LIST BOX</h1>
+        {   
+          
+    
+          }
+        
+      </div>
+    );
+  }
+  
+  export default CharacterList;
+  
